32dbfa99360123de5f7d8af50c1de415f459e39a
shenqi <1090360586@qq.com>
2021-08-23 01:05:10 +0800

    1

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,8 +8,9 @@
       <el-button type='info' @click='logout'>退出</el-button>
     </el-header>
     <el-container>
-      <el-aside width="200px">
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" :unique-opened="true">
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="togglecollpse">|||</div>
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" :unique-opened="true" :collapse=isCollapse :collapse-transition="false">
         <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
           <template slot="title">
             <i :class="iconsObj[item.id]"></i>
@@ -35,12 +36,13 @@ export default {
     return {
       menulist: [],
       iconsObj: {
-        125: 'iconfont iocn-users',
+        125: 'iconfont icon-users',
         103: 'iconfont icon-tijikongjian',
         101: 'iconfont icon-shangpin',
         102: 'iconfont icon-danju',
         145: 'iconfont icon-baobiao'
-      }
+      },
+      isCollapse: false
     }
   },
   created () {
@@ -56,6 +58,10 @@ export default {
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.menulist = res.data
       console.log(res)
+    },
+
+    togglecollpse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 
@@ -98,6 +104,16 @@ export default {
 
 .iconfont {
   margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  list-style: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 
 </style>
