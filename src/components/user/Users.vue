640b9b44b70c7ee94d4c0074540ac35c5db3dec2
shenqi <1090360586@qq.com>
2021-09-23 00:33:50 +0800

    1

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -37,7 +37,7 @@
                         <el-button type="primary" icon="el-icon-edit" size='mini' @click="showEditDialog(scope.row.id)"></el-button>
                     </el-tooltip>
                     <el-tooltip class="item" effect="dark" content="删除角色" placement="top" :enterable="false">
-                        <el-button type="danger" icon="el-icon-delete" size='mini'></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size='mini' @click="removeUserById(scope.row.id)"></el-button>
                     </el-tooltip>
                     <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
                         <el-button type="warning" icon="el-icon-setting" size='mini'></el-button>
@@ -226,6 +226,25 @@ export default {
       }
       this.editForm = res.data
       this.editDialogVisible = true
+    },
+    async removeUserById (id) {
+    // 弹框
+      const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => {
+        return err
+      })
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      const { data: res } = await this.$http.delete('users/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除用户失败')
+      }
+      this.$message.success('删除用户成功')
+      this.getUserList()
     }
   }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox } from 'element-ui'
 // import {  } from 'element-ui'
 // import {} from 'element-ui'
 
@@ -28,3 +28,4 @@ Vue.use(Pagination)
 Vue.use(Dialog)
 
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
